d548ab44ebf78091c6c66d1dffcc36a0a55326ae
Anik  The Coder <jwjejjejrjr@gmail.com>
2025-09-30 01:19:04 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # ---------------- CORS Handling ----------------
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Origin'] = 'https://flamexhub.vercel.app'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     response.headers['Access-Control-Max-Age'] = '86400'
@@ -183,3 +183,4 @@ def internal_error(error):
 # For local dev only
 if __name__ == "__main__":
     app.run(debug=True)
+
